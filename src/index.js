1c9d31c9bf77115d23694cbed44cdc3fbc8ebee4
phonty29 <amedov.bekmuhamet@gmail.com>
2022-07-03 12:25:02 +0600

    reset project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import myStore from './store/myStore';
+import store from './store/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={myStore}>
+  <Provider store={store}>
 	  <React.StrictMode>
 	    <App />
 	  </React.StrictMode>
--- src/store/myStore.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import myReducer from '../reducer/myReducer';
-
-const myStore = createStore(myReducer);
-
-export default myStore;
