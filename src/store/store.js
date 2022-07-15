d7f4bc7a5c86106f961cdcaa470e1ea350dbdd0b
phonty29 <amedov.bekmuhamet@gmail.com>
2022-07-16 05:53:26 +0600

    slider 1.1

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from '../reducer/rootReducer';
-
-const store = configureStore({
-  reducer: rootReducer
-})
-
-export default store;
-
-
-
-=======
 import {configureStore} from '@reduxjs/toolkit';
 import rootReducer from '../reducer/rootReducer';
 
 const store = configureStore({
     reducer: rootReducer
 });
->>>>>>> 46e6ced37d9bc51216018d0bdf8cb525d45145b4
 
+export default store;
